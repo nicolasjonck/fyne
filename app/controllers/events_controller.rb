f157ecf77b2412d8cdd6b17469853192a19bef24
Madiha Latafi <latafimadiha@hotmail.fr>
2017-12-12 17:35:17 +0100

    routes and controller created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,61 @@
 class EventsController < ApplicationController
+
+  before_action :set_event, only:[:show, :edit, :update, :destroy]
+  before_action :set_user, only:[:show, :new, :create, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    current_user.save
+    if @event.save
+      flash[:success] = "Congrats, your event #{@event.name} has been created!"
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = current_user
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event.update(event_params)
+    @event.save
+    if @event.save
+      flash[:success] = "Your event #{@event.name} has been successfully updated"
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :user, :category, :subcategory, :start_time, :end_time, :photo, :street_address, :city, :zip_code, :state, :country)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
 end
