c416543008c9dfe337d7eb59fbb0ffc9a031c243
Madiha Latafi <latafimadiha@hotmail.fr>
2017-12-12 17:00:58 +0100

    finished event model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :city, presence: true, on: :update
+  has_many :events, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
