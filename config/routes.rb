b2522701e0309c1195226e84b026602276cdaa76
Pierre-Alexis Bayen <bayen.pierre@gmail.com>
2017-12-13 15:50:14 +0100

    end of the new/create methods for the swipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'swipes/index'
+  # get 'swipes/index', to: 'swipes#index'
+
+  # get 'swipes/new', to: 'swipes#new'
+  # get 'swipes', to: 'swipes#create'
 
   devise_for :users
 
@@ -9,4 +12,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'profile', to: 'pages#profile'
+
+  resources :swipes, only: [:index, :new, :create]
 end
