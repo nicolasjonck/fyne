82d001ee9a36d676026afce4f314878b4d8373f6
Pierre-Alexis Bayen <bayen.pierre@gmail.com>
2017-12-18 16:46:56 +0100

    end of the description, photo, and the cat in the api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215135949) do
+ActiveRecord::Schema.define(version: 20171218135201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20171215135949) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "photouploaded"
+    t.string   "description"
+    t.string   "eventful_id"
+    t.string   "venue_name"
+    t.string   "url"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
