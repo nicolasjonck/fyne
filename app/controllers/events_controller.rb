90969588a2b82fea971587c4d94dbaed2936a6b1
Nicolas Jonckheere <nicolasjonckheere22@hotmail.com>
2017-12-18 18:24:36 +0100

    maps debug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only:[:edit, :update, :destroy]
+  before_action :set_event, only:[:show, :edit, :update, :destroy]
   before_action :set_user, only:[:show, :new, :create, :edit, :update, :destroy]
 
   def index
