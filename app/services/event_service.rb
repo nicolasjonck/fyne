85dbde33c26a88f5e7204ee6a65c72b44e2d4d35
Pierre-Alexis Bayen <bayen.pierre@gmail.com>
2017-12-19 18:26:24 +0100

    pictures based on the category when no picture available on the api

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -43,17 +43,17 @@ private
 def get_events
   set_events
   set_names_of_events
-    api_events = JSON.parse(open("http://api.eventful.com/json/events/search?app_key=#{ENV["EVENTFUL"]}&page_size=10&image_sizes=block100,large,dropshadow250&l=#{@city}").read)
+    api_events = JSON.parse(open("http://api.eventful.com/json/events/search?app_key=#{ENV["EVENTFUL"]}&page_size=10&image_sizes=large,dropshadow250,blackborder500,block250&l=#{@city}").read)
     api_events["events"]["event"].each do |event|
       new_event = Event.new
         new_event.name = event["title"]
         # new_event.category = "Other"
         # new_event.subcategory = "Other"
-        if event["image"] == nil
-          new_event.photo = "http://blog.daix.com/wp-content/uploads/2012/09/fond-d-ecran-noir-iphone-5"
-        else
-          new_event.photo = event["image"]["large"]["url"]
-        end
+        # if event["image"] == nil
+        #   new_event.photo = "http://blog.daix.com/wp-content/uploads/2012/09/fond-d-ecran-noir-iphone-5"
+        # else
+        #   new_event.photo = event["image"]["block250"]["url"]
+        # end
         new_event.start_time = event["start_time"]
         new_event.end_time = event["stop_time"]
         new_event.street_address = event["venue_address"]
@@ -85,7 +85,45 @@ def get_events
         if document.css("links url").first != nil
           new_event.url = document.css("links url").first.text
         end
+
+        if event["image"] == nil
+          hash_of_images = {
+          music: 'https://picsum.photos/200/300?image=452',
+          conference: 'https://picsum.photos/200/300?image=677',
+          comedy: 'https://picsum.photos/200/300?image=395',
+          learning_education: 'https://picsum.photos/200/300?image=885',
+          family_fun_kids: 'https://picsum.photos/200/300?image=838',
+          festivals_parades: 'https://picsum.photos/200/300?image=145',
+          movies_film: 'https://picsum.photos/200/300?image=91',
+          food: 'https://picsum.photos/200/300?image=999',
+          fundraisers: 'https://picsum.photos/200/300?image=513',
+          art: 'https://picsum.photos/200/300?image=628',
+          support: 'https://picsum.photos/200/300?image=773',
+          holiday: 'https://picsum.photos/200/300?image=845',
+          books: 'https://picsum.photos/200/300?image=1010',
+          attractions: 'https://picsum.photos/200/300?image=252',
+          community: 'https://picsum.photos/200/300?image=998',
+          business: 'https://picsum.photos/200/300?image=1031',
+          singles_social: 'https://picsum.photos/200/300?image=815',
+          schools_alumni: 'https://picsum.photos/200/300?image=532',
+          clubs_associations: 'https://picsum.photos/200/300?image=526',
+          outdoors_recreation: 'https://picsum.photos/200/300?image=1057',
+          performing_arts: 'https://picsum.photos/200/300?image=453',
+          animals: 'https://picsum.photos/200/300?image=1025',
+          politics_activism: 'https://picsum.photos/200/300?image=441',
+          sales: 'https://picsum.photos/200/300?image=686',
+          science: 'https://picsum.photos/200/300?image=967',
+          religion_spirituality: 'https://picsum.photos/200/300?image=938',
+          sports: 'https://picsum.photos/200/300?image=1058',
+          other: 'https://picsum.photos/200/300?image=467',
+        }
+          new_event.photo = hash_of_images[new_event.category.to_sym]
+        else
+          new_event.photo = event["image"]["block250"]["url"]
+        end
+
         new_event.save
+
       end
     end
 end
