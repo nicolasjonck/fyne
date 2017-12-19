89b8bae4b7595e71ec3270201d0af328fab11b93
Nicolas Jonckheere <nicolasjonckheere22@hotmail.com>
2017-12-19 11:11:33 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171218145233) do
 
   # These are extensions that must be enabled in order to support this database
@@ -46,9 +45,9 @@ ActiveRecord::Schema.define(version: 20171218145233) do
     t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.string   "photouploaded"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "photouploaded"
     t.string   "description"
     t.string   "eventful_id"
     t.string   "venue_name"
